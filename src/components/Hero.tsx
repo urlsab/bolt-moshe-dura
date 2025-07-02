32115fb15921c78610c2b71b758459cccbbc0c6b
urlsab <yairsabag213@gmail.com>
2025-07-02 14:32:19 +0300

    fix path

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import homepage from '../assets/homepage.webp?url';
+import homepage from '../assets/homepage.webp';
 
 interface HeroProps {
   title?: string;
